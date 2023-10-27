405fde29ce87fcb3600cb7d00905d0feb66c26f6
roman <117850602+KISEL4KO@users.noreply.github.com>
2023-10-27 15:34:54 +0300

    Update FirstHomeWork.py

--- FirstHomeWork.py
+++ FirstHomeWork.py
@@ -1,10 +1,13 @@
 inData = input('Введите любое число: ')
 
-
-if inData[-1] == '0':
-    print(inData, 'рублей')
-elif inData[-1] == '1':
-    print(inData, 'рубль')
-elif inData[-1] < '5':
-    print(inData, 'рубля')
-else: print(inData, 'рублей')
+checkNum = float(inData)
+if checkNum / 1 :
+    if inData[-1] == '0':
+        print(inData, 'рублей')
+    elif inData[-1] == '1':
+        print(inData, 'рубль')
+    elif inData[-1] < '5':
+        print(inData, 'рубля')
+    else:
+        print(inData, 'рублей')
+else: print('Error')
